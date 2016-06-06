3917987b20ce210512ccb4fdb0c09adb8a846b1f
pinotto <tollotpietro@outlook.com>
2016-06-06 15:23:45 +0200

    okokokokfadafasfasdasdasdsdsadas

--- c4.c
+++ c4.c
@@ -3,7 +3,8 @@
 int main(void) {
     printf("Jenkins fa la build alla commit e fa il report del terminale di gradle!\n");
     return 0;
-}dasdas
+}
+//dasdas
 //8tg86
 //provaprova
 //provaprovaprova
