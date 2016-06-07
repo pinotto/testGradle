0596eb92f92e04626a3b16bbc607b1658662b12d
pinotto <tollotpietro@outlook.com>
2016-06-07 13:41:49 +0200

    cavvrwecew

--- c4.c
+++ c4.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main(void) {
-    printf("Jenkins fa la build alla commit e fa il report del terminale di gradle!\n");
+    printf("Jenkins fa la budddild alla commit e fa il report del terminale di gradle!\n");
     return 0;
 }
