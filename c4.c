357025cd1f9dc4d58ad2d483ca577579746024df
pinotto <tollotpietro@outlook.com>
2016-06-07 11:25:58 +0200

    commit miobranch

--- c4.c
+++ c4.c
@@ -2,12 +2,8 @@
 
 int main(void) {
     printf("Jenkins fa la build alla commit e fa il report del terminale di gradle!\n");
-    return 0;   
+    return 0;
 }
-//dasdas
-//8tg86
-//provaprova
-//provaprovaprova
 
-vediamo  prova
-prova
+
+mioBranch
